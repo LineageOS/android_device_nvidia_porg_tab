aa61d2cd2b4f0c0deacef20e9297cf6a378755ca
Aaron Kling <webgeek1234@gmail.com>
2021-10-12 16:13:31 -0500

    Initial commit, import and rebrand from foster_tab

---
+++ lineage_porg_tab.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration for porg_tab.
+include device/nvidia/foster/lineage.mk
+$(call inherit-product, device/nvidia/porg_tab/full_porg_tab.mk)
+
+PRODUCT_NAME := lineage_porg_tab
+PRODUCT_DEVICE := porg_tab
